c266486ee9efb11d4b48833c3397a9c3b512850e
jasn6 <110363877+jasn6@users.noreply.github.com>
2023-12-01 20:14:22 -0500

    Logout User Functionality
    Users can no logout, css still needs to be improved.

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { FaPlus } from 'react-icons/fa';
+import { FaPlus } from "react-icons/fa";
 import { FaChevronLeft } from "react-icons/fa";
 import "./Sidebar.css";
 
-const Sidebar = ({ entries, setEntries, createFunc, deleteFunc, user  }) => {
+const Sidebar = ({ entries, setEntries, createFunc, deleteFunc, user }) => {
+  const [showLogout, setShowLogout] = useState(false);
+
   const navigate = useNavigate();
+
+  const toggleModal = () => {
+    setShowLogout(!showLogout);
+  };
 
   const handleOnClick = async (entryCode) => {
     navigate(`/main/${entryCode}`);
@@ -36,13 +42,33 @@ const Sidebar = ({ entries, setEntries, createFunc, deleteFunc, user  }) => {
     }
   };
 
+  const logoutUser = async () => {
+    try {
+      const response = await fetch("http://localhost:3001/api/auth/logout", {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      });
+      if (response.ok) {
+        const data = await response.json();
+        //alert(data.message);
+        window.location.href = "/";
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="sidebar">
       <div className="sidebar-top">
         <button className="create-button" onClick={createFunc}>
-          <FaPlus/> Create
+          <FaPlus /> Create
         </button>
-        <div className="placeholder-div"><FaChevronLeft/></div>
+        <div className="placeholder-div">
+          <FaChevronLeft />
+        </div>
       </div>
       <div className="entries">
         {entries.map((entry) => (
@@ -61,14 +87,25 @@ const Sidebar = ({ entries, setEntries, createFunc, deleteFunc, user  }) => {
           </div>
         ))}
       </div>
-      <div className="user-display">
-          <div className="user-avatar">
-            <img src="/path-to-avatar-image.jpg" alt="User Avatar" />
-          </div>
-          <div className="user-info">
-            <h3 className="user-name">{user}</h3>
-          </div>
+      <div
+        className="user-display"
+        onClick={(e) => {
+          e.stopPropagation(); // Prevents the sidebar onClick from being called
+          toggleModal();
+        }}
+      >
+        <div className="user-avatar">
+          <img src="/path-to-avatar-image.jpg" alt="User Avatar" />
         </div>
+        <div className="user-info">
+          <h3 className="user-name">{user}</h3>
+        </div>
+        {showLogout && (
+          <div className="logout-overlay">
+            <button onClick={logoutUser}>Logout</button>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
